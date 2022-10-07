b9f844fa9c3c554d4b6fce56dea030c5462adecb
Changseo <changseo.jang@riiid.co>
2022-10-07 11:06:03 +0900

    get inputs

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import * as core from "@actions/core";
 
-core.warning("hello, world")
+async function run() {
+  // Load acitons input
+  const chartmuseumUrl = core.getInput("chartmuseum-url", { required: true });
+  const chartmuseumUsername = core.getInput("chartmuseum-username", { required: true });
+  const chartmuseumPassword = core.getInput("chartmuseum-password", { required: true });
+
+  core.debug(JSON.stringify({ chartmuseumUrl, chartmuseumUsername, chartmuseumPassword }));
+}
+
+run();
