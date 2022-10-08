d1e13f7fc6b3c83233dcf87930c62f5acef486ca
BeLeap <beleap@beleap.codes>
2022-10-08 14:30:01 +0900

    clear up comments

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,38 @@ import { context, getOctokit } from "@actions/github";
 import path from "path";
 
 async function run() {
-  // Load acitons input
+  core.debug("Loading actions input");
   const githubToken = core.getInput("github-token");
 
   const chartmuseumUrl = core.getInput("chartmuseum-url", { required: true });
   const chartmuseumUsername = core.getInput("chartmuseum-username", { required: true });
   const chartmuseumPassword = core.getInput("chartmuseum-password", { required: true });
 
+  core.debug("Loaded actions input");
   core.debug(JSON.stringify({ chartmuseumUrl, chartmuseumUsername, chartmuseumPassword }));
 
-  // Check event type
+  core.debug("Checking event type");
   if (context.eventName !== "push") {
     throw new Error(`${context.eventName} not supported`);
   }
 
-  // Check diff
+  core.debug("Build octokit");
   const octokit = getOctokit(githubToken);
+  core.debug("Built octokit");
+  core.debug("Request diff");
   const { data } = await octokit.rest.repos.compareCommits({
     ...context.repo,
     base: context.payload["before"],
     head: context.payload["after"],
   });
-  core.debug(JSON.stringify(data));
+  core.debug("Requested diff");
 
-  // Process diff
+  core.debug("Process diff");
+  core.debug(JSON.stringify(data.files));
   const diffingFiles = data.files ?? [];
   const diffingDirs = diffingFiles.filter(it => it.filename.includes("Chart.yaml")).map(it => path.dirname(it.filename))
-
-  core.info(JSON.stringify(diffingDirs));
+  core.debug("Processed diff");
+  core.debug(JSON.stringify(diffingDirs));
 }
 
 run();
